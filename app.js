ee43214b33a5951af6973beda1b761e7ce898c0e
shantanu0014 <pathakmickey0014@gmail.com>
2024-08-01 19:12:51 +0530

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const socketIo = require("socket.io");
 const http = require("http");
@@ -70,6 +71,7 @@ io.on("connection", function (socket) {
     io.emit("boardState", chess.fen());
 });
 
-server.listen(3000, function () {
-    console.log("listening on port 3000");
+const port = process.env.PORT || 3000;
+server.listen(port, function () {
+    console.log(`listening on port ${port}`);
 });
